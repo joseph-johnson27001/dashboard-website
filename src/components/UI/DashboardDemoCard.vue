2476e82495acea4ac421392cc56e91b41c5ebdc1
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2025-04-03 21:34:51 +0700

    Browser Tab Naming Update

--- src/components/UI/DashboardDemoCard.vue
+++ src/components/UI/DashboardDemoCard.vue
@@ -65,7 +65,7 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.3);
   border-radius: 4px;
   opacity: 0;
   transition: opacity 0.2s ease;
--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,37 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomePage.vue";
 import Single from "../views/SinglePage.vue";
 import Multi from "../views/MultiPage.vue";
-import Hire from "..//views/HirePage.vue";
+import Hire from "../views/HirePage.vue";
 import API from "../views/APIReadyPage.vue";
 
 const routes = [
-  { path: "/", component: Home },
-  { path: "/single-page-dashboards", component: Single },
-  { path: "/multi-page-dashboards", component: Multi },
-  { path: "/hire", component: Hire },
-  { path: "/api-ready-dashboards", component: API },
+  { path: "/", component: Home, meta: { title: "Home" } },
+  {
+    path: "/single-page-dashboards",
+    component: Single,
+    meta: { title: "Single Page Dashboards" },
+  },
+  {
+    path: "/multi-page-dashboards",
+    component: Multi,
+    meta: { title: "Multi Page Dashboards" },
+  },
+  { path: "/hire", component: Hire, meta: { title: "Hire" } },
+  {
+    path: "/api-ready-dashboards",
+    component: API,
+    meta: { title: "API Ready Dashboards" },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `Template Dashboards - ${to.meta.title || "Loading..."}`;
+  next();
 });
 
 export default router;
