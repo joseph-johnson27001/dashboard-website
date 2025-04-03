83641578748970ff1a1c2627da4f48861cab68fb
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2025-04-03 15:07:00 +0700

    Nav active tab styles

--- src/components/Navigation/TopNavigation.vue
+++ src/components/Navigation/TopNavigation.vue
@@ -5,10 +5,16 @@
         <i class="fas fa-chart-bar logo-icon"></i>
       </router-link>
       <div class="nav-links">
-        <router-link to="/single">Single Page</router-link>
-        <router-link to="/multi">Multi-Page</router-link>
-        <router-link to="/api-ready">API Ready</router-link>
-        <router-link to="/hire">Hire</router-link>
+        <router-link to="/single" active-class="active-link"
+          >Single Page</router-link
+        >
+        <router-link to="/multi" active-class="active-link"
+          >Multi-Page</router-link
+        >
+        <router-link to="/api-ready" active-class="active-link"
+          >API Ready</router-link
+        >
+        <router-link to="/hire" active-class="active-link">Hire</router-link>
       </div>
     </div>
   </nav>
@@ -27,6 +33,7 @@ nav {
   align-items: center;
   padding: 15px 20px;
   border-bottom: 1px solid rgba(193, 191, 214, 0.5);
+  height: 25px;
 }
 
 .nav-container {
@@ -52,5 +59,13 @@ a {
   display: flex;
   font-size: 15px;
   gap: 1rem;
+}
+
+.active-link {
+  color: #006ba6;
+  font-weight: 500;
+  border-bottom: 2px solid #006ba6;
+  padding-bottom: 3px;
+  box-sizing: border-box;
 }
 </style>
