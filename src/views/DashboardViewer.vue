c7abed7af5d05d91016bbdeb29186e00aa1ee4d8
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2025-04-09 18:31:43 +0700

    Removed comments

--- src/views/DashboardViewer.vue
+++ src/views/DashboardViewer.vue
@@ -9,22 +9,20 @@ export default {
   name: "DashboardViewer",
   computed: {
     iframeUrl() {
-      // Get the slug from the route
       const slug = this.$route.params.slug;
 
       // Map the slug to its corresponding Netlify URL
       const dashboardUrls = {
-        healthcare: "https://your-netlify-link/healthcare", // replace with actual URLs
+        healthcare: "https://your-netlify-link/healthcare",
         logistics: "https://joseph-johnson-logistics-demo.netlify.app/",
-        "code-camp": "https://your-netlify-link/codecamp", // replace with actual
+        "code-camp": "https://your-netlify-link/codecamp",
         finance: "https://your-netlify-link/finance",
         "hr-dashboard": "https://your-netlify-link/hr",
         sales: "https://your-netlify-link/sales",
         worksphere: "https://your-netlify-link/worksphere",
       };
 
-      // Return the URL for the iframe based on the slug, or fallback to an error page
-      return dashboardUrls[slug] || "https://your-netlify-link/error"; // fallback URL if slug is not found
+      return dashboardUrls[slug] || "https://your-netlify-link/error";
     },
   },
 };
@@ -32,16 +30,16 @@ export default {
 
 <style scoped>
 .dashboard-viewer {
-  width: 100vw; /* Full viewport width */
-  height: 100vh; /* Full viewport height */
-  margin: 0; /* Remove any default margin */
-  padding: 0; /* Remove any default padding */
-  overflow: hidden; /* Hide any overflow if the iframe content is larger than the viewport */
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
 }
 
 .dashboard-iframe {
-  width: 100%; /* Make the iframe take up the full width */
-  height: 100%; /* Make the iframe take up the full height */
-  border: none; /* Remove the border of the iframe */
+  width: 100%;
+  height: 100%;
+  border: none;
 }
 </style>
