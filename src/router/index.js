b0990484ff43a5bd4469516a0b0eea5c0df7a41a
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2025-04-17 00:04:14 +0700

    Sitemap

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomePage.vue";
-import Single from "../views/SinglePage.vue";
-import Multi from "../views/MultiPage.vue";
+// import Single from "../views/SinglePage.vue";
+// import Multi from "../views/MultiPage.vue";
 import Contact from "../views/ContactPage.vue";
 import HowItWorks from "../views/HowItWorksPage.vue";
-import API from "../views/APIReadyPage.vue";
+// import API from "../views/APIReadyPage.vue";
 import Dashboards from "../views/DashboardsPage.vue";
 import DashboardViewer from "@/views/DashboardViewer.vue";
 
@@ -14,22 +14,22 @@ const routes = [
     component: Home,
     meta: { title: "Prebuilt Frontend Dashboard Templates for Web Apps" },
   },
-  {
-    path: "/single-page-dashboards",
-    component: Single,
-    meta: { title: "Single Page Dashboards" },
-  },
-  {
-    path: "/multi-page-dashboards",
-    component: Multi,
-    meta: { title: "Multi Page Dashboards" },
-  },
+  // {
+  //   path: "/single-page-dashboards",
+  //   component: Single,
+  //   meta: { title: "Single Page Dashboards" },
+  // },
+  // {
+  //   path: "/multi-page-dashboards",
+  //   component: Multi,
+  //   meta: { title: "Multi Page Dashboards" },
+  // },
   { path: "/contact", component: Contact, meta: { title: "Contact" } },
-  {
-    path: "/api-ready-dashboards",
-    component: API,
-    meta: { title: "API Ready Dashboards" },
-  },
+  // {
+  //   path: "/api-ready-dashboards",
+  //   component: API,
+  //   meta: { title: "API Ready Dashboards" },
+  // },
   {
     path: "/how-it-works",
     component: HowItWorks,
