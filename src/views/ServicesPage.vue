c07bb6eea5b31860a9a678b0d1cd228cd1e6ddaf
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2025-05-19 11:07:44 +0700

    Heading area services page

--- src/views/ServicesPage.vue
+++ src/views/ServicesPage.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>SERVICES PAGE</div>
+  <div class="services-page">
+    <h1>SERVICES</h1>
+  </div>
 </template>
+
+<style scoped>
+.services-page {
+  opacity: 0;
+  animation: fadeIn 0.5s forwards;
+}
+
+h1 {
+  font-family: "Unica One", cursive;
+  font-size: 24px;
+  margin-bottom: 10px;
+  color: #006ba6;
+  margin-top: 10px;
+  letter-spacing: -1px;
+  font-weight: 400;
+}
+
+/* Fade-in Animation */
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
