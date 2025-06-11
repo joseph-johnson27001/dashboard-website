0eec2b6bd795b0ccfa271b11fdbfdbf6b4ca67a3
Joe Johnson <joseph.johnson27001@gmail.com>
2025-06-11 11:36:02 +0700

    Resource page font size update

--- src/views/ResourcesPage.vue
+++ src/views/ResourcesPage.vue
@@ -250,10 +250,10 @@ h1 {
 }
 
 .article-content p {
-  font-size: 17px;
+  font-size: 15px;
   margin-top: 10px;
   margin-bottom: 15px;
-  color: #222;
+  color: #333;
   font-weight: 300;
   line-height: 1.4;
   font-family: "Assistant";
